0f53c5a0e28293763d321b3f3bb95a86fa5a65ab
Bonniejoy01 <joytobias97@yahoo.com>
2022-06-21 13:28:42 +0100

    JS intro task

---
+++ script.js
@@ -0,0 +1,7 @@
+var fullName = 'Joy Tobias',
+    height = '173cm',
+    country = 'Nigeria';
+
+message = 'I am ' + fullName + ' ' + 'whose height is' + ' ' + height + ' ' + 'from ' + ' ' + country + '.';   
+
+alert(message);
